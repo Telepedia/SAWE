60698f7ab738d1729fdf0a5154c8c41f196c6ea1
OriginalAuthority <luke@telepedia.net>
2025-02-12 00:16:29 +0000

    Refactor the Profiles class to be static so that it remains for the lifetime

--- Functions/Profiles/AWBProfiles.cs
+++ Functions/Profiles/AWBProfiles.cs
@@ -4,38 +4,51 @@ using System.Threading.Tasks;
 
 namespace Functions.Profiles
 {
-    public class AWBProfiles
+    public static class AWBProfiles
     {
+
         /// <summary>
-        /// Hold all of the profiles; placeholder for now
+        /// Static member so that is available for the lifetime of the application
+        /// and will not be recycled by the garbage collector (hopefully)
         /// </summary>
-        public List<Profile> Profiles { get; set; } = new List<Profile>
+        private static List<Profile> _profiles = [];
+
+
+        /// <summary>
+        /// Load all of the profiles onto the instance. This is called during app startup
+        /// </summary>
+        /// <returns></returns>
+        public static async Task LoadProfilesAsync()
         {
-            new Profile { ID = 1, Username = "Alice", Password = "PasswordAlice" },
-            new Profile { ID = 2, Username = "Bob", Password = "PasswordBob" },
-            new Profile { ID = 3, Username = "Charlie", Password = "PasswordCharlie" }
-        };
+           
+            _profiles = new List<Profile>
+            {
+                new Profile { ID = 1, Username = "Alice", Password = "PasswordAlice" },
+                new Profile { ID = 2, Username = "Bob", Password = "PasswordBob" },
+                new Profile { ID = 3, Username = "Charlie", Password = "PasswordCharlie" }
+            };
+
+            // temp sim of async work heh
+            await Task.CompletedTask;
+        }
 
         /// <summary>
         /// Return the Profiles property
         /// </summary>
         /// <returns></returns>
-        public List<Profile> GetProfiles()
-        {
-            return Profiles;
-        }
+        public static List<Profile> GetProfiles() => _profiles;
 
         /// <summary>
         /// Removes a profile from the list and returns a task indicating success.
         /// </summary>
         /// <param name="profile"></param>
         /// <returns></returns>
-        public Task<bool> DeleteProfile(Profile profile)
+        public static Task<bool> DeleteProfile(Profile profile)
         {
             // only do something if the profile is actually in the list
-            if (Profiles.Contains(profile))
+            if (_profiles.Contains(profile))
             {
-                Profiles.Remove(profile);
+                _profiles.Remove(profile);
                 return Task.FromResult(true);
             }
 

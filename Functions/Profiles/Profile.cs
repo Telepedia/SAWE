2e546db5edbf5790e36c1feae3678cf2ff71f9fa
OriginalAuthority <luke@telepedia.net>
2025-02-13 21:20:17 +0000

    Change profile model; not sure if this is 100% secure we may have to rethink?

--- Functions/Profiles/Profile.cs
+++ Functions/Profiles/Profile.cs
@@ -5,4 +5,6 @@ public class Profile
     public int ID { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public byte[] EncryptedPassword { get; set; }
+    public byte[] IV { get; set; } 
 }

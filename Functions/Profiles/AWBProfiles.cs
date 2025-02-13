7bc635c62d2c006e54ef98fee7a735d78fece857
OriginalAuthority <luke@telepedia.net>
2025-02-13 21:21:44 +0000

    Modify AWBProfiles to remove boilerplate and actually save and get profiles

--- Functions/Profiles/AWBProfiles.cs
+++ Functions/Profiles/AWBProfiles.cs
@@ -1,4 +1,5 @@
 using AWBv2.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Functions.Profiles
 {
@@ -11,24 +12,60 @@ namespace Functions.Profiles
         /// </summary>
         private static List<Profile> _profiles = [];
 
+        /// <summary>
+        /// The location of the database; uses Globals.GetDataDirPath() which will account for
+        /// different OS'
+        /// </summary>
+        /// <returns></returns>
+        private static string GetDatabasePath() => Path.Combine(Globals.GetDataDirPath(), "awbv2.db");
 
+        /// <summary>
+        /// Get the encryption key from the EncryptionHelper
+        /// </summary>
+        /// <returns></returns>
+        private static byte[]? GetEncryptionKey() => EncryptionHelper.GetEncryptionKey();
+        
         /// <summary>
         /// Load all of the profiles onto the instance. This is called during app startup
         /// </summary>
         /// <returns></returns>
         public static async Task LoadProfilesAsync()
         {
-           
-            _profiles = new List<Profile>
-            {
-                new Profile { ID = 1, Username = "Alice", Password = "PasswordAlice" },
-                new Profile { ID = 2, Username = "Bob", Password = "PasswordBob" },
-                new Profile { ID = 3, Username = "Charlie", Password = "PasswordCharlie" }
-            };
+            string dbPath = GetDatabasePath();
 
-            // temp sim of async work heh
-            await Task.CompletedTask;
+            if (!File.Exists(dbPath))
+            {
+                throw new Exception("Database not found. An error occurred during the first run of the application.");
+            }
+
+            _profiles = new List<Profile>();
+
+            await using var connection = new SqliteConnection($"Data Source={dbPath}");
+            await connection.OpenAsync();
+
+            await using var command = connection.CreateCommand();
+            command.CommandText = "SELECT Id, Username, Password, IV FROM Profiles";
+
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                byte[] encryptedPassword = (byte[])reader["Password"];
+                byte[] iv = (byte[])reader["IV"];
+                byte[] key = GetEncryptionKey() ?? throw new Exception("Encryption key not found.");
+
+                string decryptedPassword = EncryptionHelper.Decrypt(encryptedPassword, iv, key);
+
+                _profiles.Add(new Profile
+                {
+                    ID = reader.GetInt32(0),
+                    Username = reader.GetString(1),
+                    Password = decryptedPassword,
+                    EncryptedPassword = encryptedPassword,
+                    IV = iv
+                });
+            }
         }
+
 
         /// <summary>
         /// Return the Profiles property
@@ -41,16 +78,77 @@ namespace Functions.Profiles
         /// </summary>
         /// <param name="profile"></param>
         /// <returns></returns>
-        public static Task<bool> DeleteProfile(Profile profile)
+        public static async Task<bool> DeleteProfile(Profile profile)
         {
-            // only do something if the profile is actually in the list
-            if (_profiles.Contains(profile))
+            if (!_profiles.Contains(profile)) return false;
+
+            _profiles.Remove(profile);
+
+            string dbPath = GetDatabasePath();
+            await using var connection = new SqliteConnection($"Data Source={dbPath}");
+            await connection.OpenAsync();
+
+            await using var command = connection.CreateCommand();
+            command.CommandText = "DELETE FROM Profiles WHERE Id = @id";
+            command.Parameters.AddWithValue("@id", profile.ID);
+
+            await command.ExecuteNonQueryAsync();
+            return true;
+        }
+        
+        /// <summary>
+        /// Save a profile to the database
+        /// </summary>
+        /// <param name="profile"></param>
+        /// <returns></returns>
+        public static async Task<bool> Save(Profile profile)
+        {
+            string dbPath = GetDatabasePath();
+            await using var connection = new SqliteConnection($"Data Source={dbPath}");
+            await connection.OpenAsync();
+
+            bool exists = _profiles.Any(p => p.ID == profile.ID);
+            await using var command = connection.CreateCommand();
+
+            byte[] key = GetEncryptionKey() ?? throw new Exception("Encryption key not found.");
+            var (cipherText, iv) = EncryptionHelper.Encrypt(profile.Password, key);
+
+            profile.EncryptedPassword = cipherText;
+            profile.IV = iv;
+
+            if (exists)
             {
-                _profiles.Remove(profile);
-                return Task.FromResult(true);
+                command.CommandText = @"
+                    UPDATE Profiles 
+                    SET Username = @username, Password = @password, IV = @iv
+                    WHERE Id = @id";
+
+                command.Parameters.AddWithValue("@id", profile.ID);
+            }
+            else
+            {
+                command.CommandText = @"
+                    INSERT INTO Profiles (Username, Password, IV) 
+                    VALUES (@username, @password, @iv);
+                    SELECT last_insert_rowid();";
             }
 
-            return Task.FromResult(false);
+            command.Parameters.AddWithValue("@username", profile.Username);
+            command.Parameters.AddWithValue("@password", profile.EncryptedPassword);
+            command.Parameters.AddWithValue("@iv", profile.IV);
+
+            if (exists)
+            {
+                await command.ExecuteNonQueryAsync();
+            }
+            else
+            {
+                profile.ID = Convert.ToInt32(await command.ExecuteScalarAsync());
+                _profiles.Add(profile);
+            }
+
+            return true;
         }
+
     }
 }

5f7e07b1b63c10a6d9adfc9f72350b85d212119b
OriginalAuthority <luke@telepedia.net>
2025-02-23 00:30:32 +0000

    Disable interaction on checkboxes until implentation is complete

--- AWBv2/ViewModels/ProcessOptionsViewModel.cs
+++ AWBv2/ViewModels/ProcessOptionsViewModel.cs
@@ -8,12 +8,12 @@ public class ProcessOptionsViewModel : ReactiveObject
     /// <summary>
     /// Should we do general fixes on the pages in the list?
     /// </summary>
-    [Reactive] public bool GeneralFixes { get; set; } = true;
+    [Reactive] public bool GeneralFixes { get; set; } = false;
     
     /// <summary>
     /// Should we convert unicode to their proper symbols? 
     /// </summary>
-    [Reactive] public bool UnicodifyWholePage { get; set; } = true;
+    [Reactive] public bool UnicodifyWholePage { get; set; } = false;
     
     /// <summary>
     /// Should we enable find and replace? (This toggle is for both standard F&R and the template subst)
